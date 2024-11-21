39daa39f6af83bb6bfe8d49fe73769de32357936
Sahil Sharma <vlsharma713@gmail.com>
2024-11-21 14:58:00 +0530

    Remove nvchad

---
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,24 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'goolord/alpha-nvim',
+    dependencies = { 'echasnovski/mini.icons' },
+    config = function()
+      require('alpha').setup(require('alpha.themes.startify').config)
+    end,
+  },
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup {}
+    end,
+  },
+}
