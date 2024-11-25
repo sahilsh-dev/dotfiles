5c9fa887e695611cb33767ec02c1a0b2ce9991af
Sahil Sharma <vlsharma713@gmail.com>
2024-11-25 15:39:58 +0530

    Minor fixes

--- awesome/.config/awesome/modules/bar.lua
+++ awesome/.config/awesome/modules/bar.lua
@@ -1,6 +1,5 @@
 local awful = require("awful")
 local gears = require("gears")
-local beautiful = require("beautiful")
 local lain = require("lain")
 local wibox = require("wibox")
 
@@ -46,21 +45,6 @@ local tasklist_buttons = gears.table.join(
 	end)
 )
 
-local function set_wallpaper(s)
-	-- Wallpaper
-	if beautiful.wallpaper then
-		local wallpaper = beautiful.wallpaper
-		-- If wallpaper is a function, call it with the screen
-		if type(wallpaper) == "function" then
-			wallpaper = wallpaper(s)
-		end
-		gears.wallpaper.maximized(wallpaper, s, true)
-	end
-end
-
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
-
 -- Widgets
 myvolume = lain.widget.pulse({
 	timeout = 30,
@@ -76,6 +60,7 @@ myvolume = lain.widget.pulse({
 		end
 	end,
 })
+
 mybrightness = lain.widget.brt({
 	timeout = 30,
 	settings = function()
--- awesome/.config/awesome/modules/signals.lua
+++ awesome/.config/awesome/modules/signals.lua
@@ -3,6 +3,21 @@ local awful = require("awful")
 local beautiful = require("beautiful")
 local wibox = require("wibox")
 
+local function set_wallpaper(s)
+	-- Wallpaper
+	if beautiful.wallpaper then
+		local wallpaper = beautiful.wallpaper
+		-- If wallpaper is a function, call it with the screen
+		if type(wallpaper) == "function" then
+			wallpaper = wallpaper(s)
+		end
+		gears.wallpaper.maximized(wallpaper, s, true)
+	end
+end
+
+-- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
+screen.connect_signal("property::geometry", set_wallpaper)
+
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function(c)
 	-- Set the windows at the slave,
