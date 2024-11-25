20b9c2ef8d028fb06fa47b1e5b13cfa01b8a4b64
Sahil Sharma <vlsharma713@gmail.com>
2024-11-26 02:03:43 +0530

    Add harpoon

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -102,7 +102,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>g', group = '[G]it hunk', mode = { 'n', 'v' } },
       },
     },
   },
