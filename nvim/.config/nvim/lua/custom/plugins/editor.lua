b9b69fdaeb9f36fd35134c85355fcf6deba37cfe
Sahil Sharma <vlsharma713@gmail.com>
2025-02-20 23:27:18 +0530

    Remove harpoon
    Sorry Primeagen

--- nvim/.config/nvim/lua/custom/plugins/editor.lua
+++ nvim/.config/nvim/lua/custom/plugins/editor.lua
@@ -38,43 +38,6 @@ return {
     end,
   },
   {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local harpoon = require 'harpoon'
-      harpoon:setup()
-
-      vim.keymap.set('n', '<leader>m', function()
-        harpoon:list():add()
-      end, { desc = 'Add to harpoon list' })
-      vim.keymap.set('n', '<leader>h', function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end, { desc = 'Toggle harpoon list' })
-
-      vim.keymap.set('n', '<A-1>', function()
-        harpoon:list():select(1)
-      end)
-      vim.keymap.set('n', '<A-2>', function()
-        harpoon:list():select(2)
-      end)
-      vim.keymap.set('n', '<A-3>', function()
-        harpoon:list():select(3)
-      end)
-      vim.keymap.set('n', '<A-4>', function()
-        harpoon:list():select(4)
-      end)
-
-      -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set('n', '<C-S-P>', function()
-        harpoon:list():prev()
-      end)
-      vim.keymap.set('n', '<C-S-N>', function()
-        harpoon:list():next()
-      end)
-    end,
-  },
-  {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
     ---@module 'render-markdown'
